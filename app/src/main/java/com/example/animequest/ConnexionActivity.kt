8ec1d4b2f10ff6f995787c7be0b371bcf9b08e57
akia-web <59091758+akia-web@users.noreply.github.com>
2024-09-23 12:30:52 +0200

    connexion page

--- app/src/main/java/com/example/animequest/ConnexionActivity.kt
+++ app/src/main/java/com/example/animequest/ConnexionActivity.kt
@@ -1,32 +1,16 @@
 package com.example.animequest
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-    lateinit var connexionButton: Button
-
+class ConnexionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-
-        connexionButton = findViewById(R.id.connexion)
-
-        connexionButton.setOnClickListener {
-            // Créer un Intent pour ouvrir une nouvelle activité
-            val intent = Intent(this, ConnexionActivity::class.java)
-            startActivity(intent)  // Démarrer la nouvelle activité
-        }
-
-
+        setContentView(R.layout.activity_connexion)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
